f9a07153760e1a14ac8a7dadfc4abb1e430a78b6
Philippe JOULOT <phjou@smile.fr>
2017-03-02 17:27:43 +0100

    Change the block to use hi own config for the sku

--- src/Plugin/Block/ProductBlock.php
+++ src/Plugin/Block/ProductBlock.php
@@ -3,6 +3,8 @@
 namespace Drupal\smile_demo\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Provides a 'Product' Block.
@@ -12,16 +14,48 @@ use Drupal\Core\Block\BlockBase;
  *   admin_label = @Translation("Product block"),
  * )
  */
-class ProductBlock extends BlockBase {
+class ProductBlock extends BlockBase implements BlockPluginInterface {
 
   /**
    * {@inheritdoc}
    */
   public function build() {
+    $config = $this->getConfiguration();
+
+    $sku = '';
+    if (!empty($config['product_id'])) {
+      $sku = $config['product_id'];
+    }
+
     return array(
       '#theme' => 'block_esi',
-      '#product_id' => 'berghoff_3600381',
+      '#product_id' => $sku,
     );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['product_sku'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('SKU'),
+      '#description' => $this->t('The product identifier'),
+      '#default_value' => isset($config['product_id']) ? $config['product_id'] : '',
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['product_id'] = $form_state->getValue('product_sku');
   }
 
 }
